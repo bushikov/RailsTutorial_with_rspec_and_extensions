4ce6c53e5dd8ae0bec3c7fa4b9a7bf9b10c3751b
bushikov <bushikov@noreplay.com>
2017-09-25 18:57:44 +0900

    Added messages table to DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170921044227) do
+ActiveRecord::Schema.define(version: 20170925095252) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "sender_id"
+    t.integer  "receiver_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["receiver_id"], name: "index_messages_on_receiver_id"
+    t.index ["sender_id"], name: "index_messages_on_sender_id"
+  end
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
---
+++ spec/factories/messages.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :message do
+    content "MyText"
+    sender_id 1
+    receiver_id 1
+  end
+end
